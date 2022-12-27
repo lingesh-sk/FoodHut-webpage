cc946ebd2cc1265fc4f5361b851789b4d004ee15
lingesh-sk <119925929+lingesh-sk@users.noreply.github.com>
2022-12-27 12:57:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
 	header.classList.toggle("sticky", window.scrollY > 0);
-	console.log("ghello")
+	
 });
 
 let menu =  document.querySelector('#menu-icon');
